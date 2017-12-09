cc676736f5160e6d128ee10c2c37ee04148610df
Samat Karymshakov <bskordo@gmail.com>
2017-12-09 19:29:54 +0600

    Fixed style guide issues

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
+from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for, escape, request
 from flask import make_response
 from init import app, db
 from model import Post
-from datetime import datetime
 
 
 def get_user_hash():
@@ -19,15 +19,14 @@ def form():
     return render_template('form.html')
 
 
-@app.route('/post/<url_id>',methods=['GET'])
+@app.route('/post/<url_id>', methods=['GET'])
 def post(url_id):
-    post = Post.query.filter_by(url_id = url_id).first()
+    post = Post.query.filter_by(url_id=url_id).first()
     owner_hash = get_user_hash()
     if post.user_hash == owner_hash:
-        return render_template('edit.html',post=post)
+        return render_template('edit.html', post=post)
     else:
-        return render_template('post.html',post=post)
-    
+        return render_template('post.html', post=post)
 
 
 @app.route('/create', methods=['POST'])
@@ -45,19 +44,18 @@ def create():
     return response
 
 
-
 @app.route('/edit/<url_id>', methods=['POST'])
 def edit(url_id):
-    post = Post.query.filter_by(url_id = url_id).first()
+    post = Post.query.filter_by(url_id=url_id).first()
     post.header = request.form['header']
     post.signature = request.form['signature']
     post.body = request.form['body']
     post.url_id = url_id
     user_hash = get_user_hash()
-    post = Post(post.header, post.signature, post.body, url_id, user_hash)
+    post = Post(post.header, post.signature, post.body,
+                url_id, user_hash)
     db.session.commit()
     return redirect(url_for('post',url_id=post.url_id))
-
 
 
 if __name__ == "__main__":
